698a242ef99f42c5adaf787f4e1dd20fb837987a
sol namgung <nandaras0103@gmail.com>
2022-05-18 20:48:22 +0900

    first commit

---
+++ src/main/java/com/example/flowerDelivery/FlowerDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowerDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerDeliveryApplication.class, args);
+	}
+
+}
